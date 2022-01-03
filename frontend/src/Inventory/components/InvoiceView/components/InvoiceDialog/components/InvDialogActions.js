bb2c7e507f27b0626ff2b6bf18b8fe9673e1291b
ShianWeiLiew <96806137+ShianWeiLiew@users.noreply.github.com>
2022-01-03 15:43:14 +0800

    Removed start icon from 'Modify' button
    Removed start icon from 'Modify' button in invoice dialog.

--- frontend/src/Inventory/components/InvoiceView/components/InvoiceDialog/components/InvDialogActions.js
+++ frontend/src/Inventory/components/InvoiceView/components/InvoiceDialog/components/InvDialogActions.js
@@ -6,11 +6,11 @@ import LoadingButton from "@mui/lab/LoadingButton";
 // ICONS
 import SendRoundedIcon from "@mui/icons-material/SendRounded";
 import RestartAltRoundedIcon from "@mui/icons-material/RestartAltRounded";
-import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
-import EditRoundedIcon from '@mui/icons-material/EditRounded';
-import InventoryRoundedIcon from '@mui/icons-material/InventoryRounded';
-import EngineeringRoundedIcon from '@mui/icons-material/EngineeringRounded';
-import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+import CancelRoundedIcon from "@mui/icons-material/CancelRounded";
+import EditRoundedIcon from "@mui/icons-material/EditRounded";
+import InventoryRoundedIcon from "@mui/icons-material/InventoryRounded";
+import EngineeringRoundedIcon from "@mui/icons-material/EngineeringRounded";
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
 
 const InvDialogActions = (props) => {
   return (
@@ -71,7 +71,6 @@ const InvDialogActions = (props) => {
               onClick={props.submitHandler}
               disabled={!props.modifiable}
               size="large"
-              startIcon={<EditRoundedIcon />}
             >
               Modify
             </LoadingButton>
@@ -112,7 +111,13 @@ const InvDialogActions = (props) => {
               color={props.status === "Active" ? "success" : "warning"}
               onClick={props.returnToggleHandler}
               size="large"
-              endIcon={props.status === "Active" ? (<InventoryRoundedIcon />) : (<EngineeringRoundedIcon />)}
+              endIcon={
+                props.status === "Active" ? (
+                  <InventoryRoundedIcon />
+                ) : (
+                  <EngineeringRoundedIcon />
+                )
+              }
             >
               {props.status === "Active"
                 ? "Mark as Returned"
