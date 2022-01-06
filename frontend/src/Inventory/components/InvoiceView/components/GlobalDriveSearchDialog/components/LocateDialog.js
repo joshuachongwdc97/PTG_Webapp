76b2260e52de66bd3506e3e313471ac85a74e424
joshuachongwdc97 <jia.hao.chong@wdc.com>
2022-01-06 12:23:52 +0800

    Added Animation to Drive Location Dialog

--- frontend/src/Inventory/components/InvoiceView/components/GlobalDriveSearchDialog/components/LocateDialog.js
+++ frontend/src/Inventory/components/InvoiceView/components/GlobalDriveSearchDialog/components/LocateDialog.js
@@ -3,6 +3,7 @@ import { Divider, Grid } from "@mui/material";
 
 // ANIMATIONS
 import GIF from "../../../../../../Shared/assets/comp.gif";
+import Animate from "../../../../../../Shared/transitions/Animate";
 
 // COMPONENTS
 import Dialog from "../../../../../../Shared/components/Dialog/Dialog";
@@ -50,7 +51,9 @@ const LocateDialog = (props) => {
         <Dialog open={props.open} close={props.close} maxWidth="xs">
           <Grid container spacing={1.5}>
             <Grid item xs={12}>
-              <MediaCard media={GIF} height={220} />
+              <Animate show>
+                <MediaCard media={GIF} height={220} />
+              </Animate>
             </Grid>
             <Grid item xs={12}>
               <Divider />
@@ -59,125 +62,133 @@ const LocateDialog = (props) => {
             {driveLocation ? (
               <React.Fragment>
                 <Grid item xs={12}>
-                  <OutlinedCard click>
-                    <Grid
-                      container
-                      spacing={2}
-                      justifyContent={"center"}
-                      alignItems={"center"}
-                    >
-                      <Grid item xs={2} align="right">
-                        <Avatar
-                          variant="rounded"
-                          sx={{ width: 35, height: 35 }}
-                        >
-                          <DesktopWindowsRoundedIcon />
-                        </Avatar>
-                      </Grid>
-                      <Grid item xs={10} align="center">
-                        <Typography
-                          variant="subtitle2"
-                          display="block"
-                          sx={{ fontSize: "16px" }}
-                        >
-                          {driveLocation.sysId}
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </OutlinedCard>
-                </Grid>
-                <Grid item xs={12}>
-                  <OutlinedCard click>
-                    <Grid
-                      container
-                      spacing={2}
-                      justifyContent={"center"}
-                      alignItems={"center"}
-                    >
-                      <Grid item xs={2} align="right">
-                        <Avatar
-                          variant="rounded"
-                          sx={{ width: 35, height: 35 }}
-                        >
-                          <MyLocationRoundedIcon />
-                        </Avatar>
-                      </Grid>
-                      <Grid item xs={10} align="center">
-                        <Typography
-                          variant="subtitle2"
-                          display="block"
-                          sx={{ fontSize: "16px" }}
-                        >
-                          {"Rack " +
-                            driveLocation.rackNo +
-                            " Row " +
-                            driveLocation.rackRow +
-                            " --- " +
-                            driveLocation.rackLoc}
-                        </Typography>
-                      </Grid>
-                    </Grid>
-                  </OutlinedCard>
-                </Grid>
-                <Grid item xs={12}>
-                  <OutlinedCard click>
-                    <Grid
-                      container
-                      spacing={2}
-                      justifyContent={"center"}
-                      alignItems={"center"}
-                    >
-                      <Grid item xs={2} align="right">
-                        <Avatar
-                          variant="rounded"
-                          sx={{ width: 35, height: 35 }}
-                        >
-                          <HttpRoundedIcon />
-                        </Avatar>
-                      </Grid>
-                      {driveLocation.ip && (
+                  <Animate show delay="0.4s">
+                    <OutlinedCard click>
+                      <Grid
+                        container
+                        spacing={2}
+                        justifyContent={"center"}
+                        alignItems={"center"}
+                      >
+                        <Grid item xs={2} align="right">
+                          <Avatar
+                            variant="rounded"
+                            sx={{ width: 35, height: 35 }}
+                          >
+                            <DesktopWindowsRoundedIcon />
+                          </Avatar>
+                        </Grid>
                         <Grid item xs={10} align="center">
                           <Typography
                             variant="subtitle2"
                             display="block"
                             sx={{ fontSize: "16px" }}
                           >
-                            {driveLocation.ip}
+                            {driveLocation.sysId}
                           </Typography>
                         </Grid>
-                      )}
-                    </Grid>
-                  </OutlinedCard>
+                      </Grid>
+                    </OutlinedCard>
+                  </Animate>
+                </Grid>
+                <Grid item xs={12}>
+                  <Animate show delay="0.8s">
+                    <OutlinedCard click>
+                      <Grid
+                        container
+                        spacing={2}
+                        justifyContent={"center"}
+                        alignItems={"center"}
+                      >
+                        <Grid item xs={2} align="right">
+                          <Avatar
+                            variant="rounded"
+                            sx={{ width: 35, height: 35 }}
+                          >
+                            <MyLocationRoundedIcon />
+                          </Avatar>
+                        </Grid>
+                        <Grid item xs={10} align="center">
+                          <Typography
+                            variant="subtitle2"
+                            display="block"
+                            sx={{ fontSize: "16px" }}
+                          >
+                            {"Rack " +
+                              driveLocation.rackNo +
+                              " Row " +
+                              driveLocation.rackRow +
+                              " --- " +
+                              driveLocation.rackLoc}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </OutlinedCard>
+                  </Animate>
+                </Grid>
+                <Grid item xs={12}>
+                  <Animate show delay="1.2s">
+                    <OutlinedCard click>
+                      <Grid
+                        container
+                        spacing={2}
+                        justifyContent={"center"}
+                        alignItems={"center"}
+                      >
+                        <Grid item xs={2} align="right">
+                          <Avatar
+                            variant="rounded"
+                            sx={{ width: 35, height: 35 }}
+                          >
+                            <HttpRoundedIcon />
+                          </Avatar>
+                        </Grid>
+                        {driveLocation.ip && (
+                          <Grid item xs={10} align="center">
+                            <Typography
+                              variant="subtitle2"
+                              display="block"
+                              sx={{ fontSize: "16px" }}
+                            >
+                              {driveLocation.ip}
+                            </Typography>
+                          </Grid>
+                        )}
+                      </Grid>
+                    </OutlinedCard>
+                  </Animate>
                 </Grid>
               </React.Fragment>
             ) : (
               <Grid item xs={12}>
-                <OutlinedCard click>
-                  <Grid
-                    container
-                    spacing={2}
-                    justifyContent={"center"}
-                    alignItems={"center"}
-                  >
-                    <Grid item xs={2} align="right">
-                      <Avatar
-                        variant="rounded"
-                        sx={{ width: 35, height: 35, bgcolor: red[500] }}
-                      >
-                        <CloseRoundedIcon />
-                      </Avatar>
+                <Animate show>
+                  <OutlinedCard click>
+                    <Grid
+                      container
+                      spacing={2}
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                    >
+                      <Grid item xs={2} align="right">
+                        <Avatar
+                          variant="rounded"
+                          sx={{ width: 35, height: 35, bgcolor: red[500] }}
+                        >
+                          <CloseRoundedIcon />
+                        </Avatar>
+                      </Grid>
+                      <Grid item xs={10} align="center">
+                        <Typography
+                          variant="subtitle2"
+                          display="block"
+                          sx={{ fontSize: "16px" }}
+                        >
+                          Unable to Locate Drive
+                        </Typography>
+                      </Grid>
                     </Grid>
-                    <Grid item xs={10} align="center">
-                      <Typography
-                        variant="subtitle2"
-                        display="block"
-                        sx={{ fontSize: "16px" }}
-                      >
-                        Unable to Locate Drive
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </OutlinedCard>
+                  </OutlinedCard>
+                </Animate>
               </Grid>
             )}
           </Grid>
