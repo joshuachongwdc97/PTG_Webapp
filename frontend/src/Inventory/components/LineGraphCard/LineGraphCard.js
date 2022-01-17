396414e63b015a1d9fd6dac7762c7769f5502e2c
ShianWeiLiew <96806137+ShianWeiLiew@users.noreply.github.com>
2022-01-17 17:22:52 +0800

    Converted selectMenu to buttons
    Converted selectMenu because certain components causes line graph animation to not transition smoothly.

--- frontend/src/Inventory/components/LineGraphCard/LineGraphCard.js
+++ frontend/src/Inventory/components/LineGraphCard/LineGraphCard.js
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
 import { ResponsiveLine } from "@nivo/line";
 import { Grid } from "@mui/material";
+import { Button } from "@mui/material";
 
 import BasicCard from "../../../Shared/components/Card/BasicCard";
 import { DarkContext } from "../../../Shared/context/dark-context";
-import SelectMenu from "../../../Shared/components/Input/SelectMenu";
 
 const LineGraphCard = (props) => {
   const darkMode = useContext(DarkContext);
@@ -31,20 +31,31 @@ const LineGraphCard = (props) => {
 
   return (
     <BasicCard>
-      <Grid item container xs={2.5}>
-        <SelectMenu
-          required
-          label="Timeline"
-          data={[
-            { value: "past week", label: "Past Week" },
-            { value: "past month", label: "Past Month" },
-            { value: "past year", label: "Past Year" },
-          ]}
-          onChange={props.changePeriodHandler}
-          value={props.selectedPeriod}
-          name="period"
+      <Grid item container xs={12}>
+        <Button
+          variant="text"
+          onClick={props.changePeriodHandler}
+          value="past 3 months"
           size="small"
-        ></SelectMenu>
+        >
+          3 Months
+        </Button>
+        <Button
+          variant="text"
+          onClick={props.changePeriodHandler}
+          value="past 6 months"
+          size="small"
+        >
+          6 Months
+        </Button>
+        <Button
+          variant="text"
+          onClick={props.changePeriodHandler}
+          value="past year"
+          size="small"
+        >
+          Year
+        </Button>
       </Grid>
 
       <Grid item container sx={{ height: "12.5em" }}>
@@ -52,7 +63,7 @@ const LineGraphCard = (props) => {
           theme={darkMode.dark ? darkTheme : lightTheme}
           data={props.data}
           colors={(id) => id.color}
-          margin={{ top: 10, right: 100, bottom: 20, left: 50 }}
+          margin={{ top: 10, right: 100, bottom: 25, left: 50 }}
           xScale={{ type: "point" }}
           yScale={{
             type: "linear",
