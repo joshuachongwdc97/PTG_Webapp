0f2ac12b10fde92fcdd15af936c3e558c6dbd5ff
ShianWeiLiew <96806137+ShianWeiLiew@users.noreply.github.com>
2022-01-19 17:13:18 +0800

    Added spacing in code

--- frontend/src/Shared/components/Card/BasicCard.js
+++ frontend/src/Shared/components/Card/BasicCard.js
@@ -4,6 +4,7 @@ import CardActions from "@mui/material/CardActions";
 import { CardActionArea } from "@mui/material";
 import CardContent from "@mui/material/CardContent";
 import { styled } from "@mui/material/styles";
+
 const BasicCard = (props) => {
   const CardContentEvenPadding = styled(CardContent)(
     `padding: 16px; &:last-child { padding-bottom: 16px; }`
