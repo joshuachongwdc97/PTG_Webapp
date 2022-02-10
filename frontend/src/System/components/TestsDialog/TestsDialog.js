8c356101d42d1c9c8a6d17b70be0b2d5c2d87fcd
ShianWeiLiew <96806137+ShianWeiLiew@users.noreply.github.com>
2022-02-10 12:21:47 +0800

    Added state to testDetCards and tests filter section

--- frontend/src/System/components/TestsDialog/TestsDialog.js
+++ frontend/src/System/components/TestsDialog/TestsDialog.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
 import BasicDialog from "../../../Shared/components/Dialog/Dialog";
 import TestDetCard from "./components/TestDetCard";
+import TestsFilter from "./components/TestsFilter";
 
 const TestsDialog = (props) => {
-  const TestDetCards = props.tests.map((test) => {
-    return (
-      <Grid item xs={3} key={test.id}>
-        <TestDetCard test={test} drvPrgms={props.drvPrgms} key={test.id} />
-      </Grid>
+  const [testDetCards, setTestDetCards] = useState([]);
+
+  const getTestDetCards = () => {
+    setTestDetCards(
+      props.tests.map((test) => {
+        return (
+          <Grid item xs={3} key={test.id}>
+            <TestDetCard test={test} drvPrgms={props.drvPrgms} key={test.id} />
+          </Grid>
+        );
+      })
     );
-  });
+  };
+
+  // manually resets filter because dialog component does not unmount on close
+  // open and close only controls the visibility
+  useEffect(() => {
+    if (props.open) {
+      getTestDetCards();
+    }
+  }, [props.open]);
 
   return (
     <BasicDialog
@@ -20,9 +35,33 @@ const TestsDialog = (props) => {
       close={props.close}
       title="Supported Tests"
       maxWidth="lg"
+      position={{ sx: { position: "fixed", top: "3%" } }}
     >
       <Grid container spacing={1.5}>
-        {TestDetCards}
+        <Grid item xs={12}>
+          <TestsFilter
+            tests={props.tests}
+            drvPrgms={props.drvPrgms}
+            setTestDetCards={setTestDetCards}
+          />
+        </Grid>
+
+        {testDetCards.length ? (
+          testDetCards
+        ) : (
+          <Grid
+            item
+            xs={12}
+            justifyContent={"center"}
+            alignItems={"center"}
+            sx={{
+              height: "250px",
+              display: "flex",
+            }}
+          >
+            <Typography variant="h6">No Tests Found</Typography>
+          </Grid>
+        )}
       </Grid>
     </BasicDialog>
   );
