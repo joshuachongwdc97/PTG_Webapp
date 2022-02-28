c063504512a00748cbd41637a95b59ca07690b1e
ShianWeiLiew <96806137+ShianWeiLiew@users.noreply.github.com>
2022-02-28 12:00:24 +0800

    Rearranged stats in order

--- frontend/src/Shared/functions/getSysSumm.js
+++ frontend/src/Shared/functions/getSysSumm.js
@@ -4,10 +4,10 @@ import sysStatus from "./sysStatus";
 const getSysSumm = (systems) => {
   const sysSummary = {
     online: 0,
-    reserved: 0,
-    offline: 0,
-    "test in progress": 0,
     "test completed": 0,
+    "test in progress": 0,
+    offline: 0,
+    reserved: 0,
   };
 
   systems.forEach((sys) => {
