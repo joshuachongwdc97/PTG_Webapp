cd0122f16dd8ac4b874d08a2eaf4962904348fdd
ShianWeiLiew <96806137+ShianWeiLiew@users.noreply.github.com>
2022-03-09 16:44:32 +0800

    Added middleware to enforce admin access

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -4,14 +4,21 @@ const { check } = require("express-validator");
 const router = express.Router();
 
 const userController = require("../controller/user-controller");
+const checkAuth = require("../middleware/check-auth").checkAuth;
+const checkRole = require("../middleware/check-role").checkRole;
+
+router.post("/login", userController.login);
+
+router.use(checkAuth);
+
+// can be applied to specific routes, e.g, router.get("/", checkRole("admin"), userController.getUsers);
+router.use(checkRole("admin"));
 
 router.get("/", userController.getUsers);
 
 router.get("/:id", userController.getUser);
 
 router.post("/add", [check("email").not().isEmpty()], userController.addUser);
-
-router.post("/login", userController.login);
 
 router.patch(
   "/:id",
